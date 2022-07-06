7a766b8a6cbb6534e158058a3e8a60791a1dff56
Eric LIAO <d1044171009@gmail.com>
2022-07-06 17:38:34 +0800

    add function  showMovieModal

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://movie-list.alphacamp.io'
-const INDEX_URL =  BASE_URL + '/api/v1/movies'
+const INDEX_URL =  BASE_URL + '/api/v1/movies/'
 const POSTER_URL = BASE_URL + '/posters/'
 
 //存放電影資料
@@ -9,24 +9,27 @@ const movies = []
 const dataPanel = document.querySelector('#data-panel')
 
 //渲染電影清單
-function renderMovieList(movies) {
+function renderMovieList(data) {
   let rawhtml = ''
-  movies.forEach(movie => {
+  movies.forEach(item => {
+    //title, image, id 
     rawhtml += `
       <div class="col-sm-3">
         <div class="mb-2">
           <div class="card">
             <img 
-              src="${POSTER_URL + movie.image}"
+              src="${POSTER_URL + item.image}"
               alt="Movie Poster">
             <div class="card-body">
-              <h5 class="card-title">${movie.title}</h5>
+              <h5 class="card-title">${item.title}</h5>
             </div>
             <div class="card-footer">
               <button 
                 class="btn btn-primary btn-show-movie"
                 data-bs-toggle="modal"
-                data-bs-target="#movie">More</button>
+                data-bs-target="#movie"
+                data-id="${item.id}"
+                >More</button>
               <button class="btn btn-info btn-add-favorite">+</button>
             </div>
           </div>
@@ -36,6 +39,33 @@ function renderMovieList(movies) {
   dataPanel.innerHTML = rawhtml
 }
 
+//function showmodal
+function showMovieModal(id) {
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
+
+  axios
+    .get(INDEX_URL + id)
+    .then(response => {
+      const data = response.data.results
+      console.log(data)
+      modalTitle.innerText = data.title
+      modalDate.innerText = 'Release date: ' + data.release_date
+      modalDescription.innerText = data.description
+      modalImage.innerHTML = `<img src="${POSTER_URL + data.image
+        }" alt="movie-poster" class="img-fluid">` 
+    })
+
+}
+
+//show modal監聽器
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.btn-show-movie')) {
+    showMovieModal(Number(event.target.dataset.id))
+  }
+})
 
 //取得電影資料
 axios
@@ -50,6 +80,5 @@ axios
     //法二 展開運算子 spread opeartor
     movies.push(...response.data.results) 
     renderMovieList(movies)
-    console.log(movies)
   })
   .catch(error => console.log(error))
